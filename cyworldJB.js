d5cf5bb8987ecd64668dc1013bff6ca48a111558
dakyumlee <oicrcutie@gmail.com>
2023-06-09 20:46:03 +0900

    Revert "검색기능 추가"
    This reverts commit 80b32018ad5a08c458bcf60ebf7e046b1e95d1b3.

--- cyworldJB.js
+++ cyworldJB.js
@@ -35,19 +35,3 @@ var progress;
         progressBar.value = (audio.currentTime / audio.duration) * 100;
       }
     }
-
-    function search() {
-      var word = document.getElementById("searchword").value;
-      var google = ["구글", "google", "Google", "GOOGLE"];
-      var naver = ["네이버", "naver", "Naver", "NAVER"];
-      var daum = ["다음", "Daum", "daum", "DAUM"];
-    
-      if (google.includes(word)) {
-        window.location.href = "http://google.com";
-      } else if (naver.includes(word)) {
-        window.location.href = "http://naver.com";
-      } else if (daum.includes(word)) {
-        window.location.href = "http://daum.net";
-      }
-        alert("잠시만 기다려주세요..");
-    }
